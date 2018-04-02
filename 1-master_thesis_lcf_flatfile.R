798dd10d4caeeb90306781a6d9437d5b8f5414fc
wizardfish123 <maikwehmeyer@gmx.net>
2018-04-02 16:48:26 +0200

    Initial overall commit

--- 1-master_thesis_lcf_flatfile.R
+++ 1-master_thesis_lcf_flatfile.R
@@ -1,8 +1,18 @@
-### Author: Laura Carolin Freitag
-### Date: 04/02/2018
-### Running time: 1 min
-
+#   -------------------------------------------------------------------------------------------
+# Author: Laura Carolin Freitag
+# June 20th, 2017
+# Script job: Create flatfile 
+# Running time: 1 min
+#   -------------------------------------------------------------------------------------------
 
 # Setup ---------------------------------------------------------------------------------------
 
+options(warn = 1) # All warning are printed as they occour
+setwd("~/git_local/master_thesis_lcf") # Set working directory
+# Load Libraries
 library("tidyverse")
+
+# Load Data -----------------------------------------------------------------------------------
+
+dat_us <- read_csv("data/ExportCountryBits_USA.csv")
+dat_germany <- read_csv("data/ExportCountryBits_germany.csv")
