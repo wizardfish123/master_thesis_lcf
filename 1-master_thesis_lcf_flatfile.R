3d8c05a16d482f7550630d2f43cde03cab467702
wizardfish123 <maikwehmeyer@gmx.net>
2018-04-02 13:18:00 +0200

    intial commit

---
+++ 1-master_thesis_lcf_flatfile.R
@@ -0,0 +1,8 @@
+### Author: Laura Carolin Freitag
+### Date: 04/02/2018
+### Running time: 1 min
+
+
+# Setup ---------------------------------------------------------------------------------------
+
+library("tidyverse")
